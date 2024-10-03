e72efbd2476a070bfadb3b663f2a239daec7b374
Deepak kumar <dee96872@gmail.com>
2024-10-03 19:20:29 +0530

    add some component for doctor list

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div className=' text-black'>
       <Header/>
+      <SpecialityMenu/>
+      <TopDoctors/>
     </div>
   )
 }
